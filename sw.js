2f9196284841036cf4fadc3ca85b0df054b3e34e
dilum97 <www.dilumhimesh34@gmail.com>
2026-06-30 07:10:42 +0530

    Change CACHE_NAME to dt-album-shell-v2
    Updated cache name to version 2 for PWA.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // and the shell loads instantly on repeat visits. Photo/video data
 // always goes to the network so the album stays up to date.
 
-const CACHE_NAME = "dt-album-shell-v1";
+const CACHE_NAME = "dt-album-shell-v2";
 const SHELL_FILES = [
   "./",
   "./index.html",
