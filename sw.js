d85a22503085fd9d65e4771a49a7f423067f9bb1
dilum97 <www.dilumhimesh34@gmail.com>
2026-07-09 01:06:43 +0530

    Update cache name to v3 in service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // and the shell loads instantly on repeat visits. Photo/video data
 // always goes to the network so the album stays up to date.
 
-const CACHE_NAME = "dt-album-shell-v2";
+const CACHE_NAME = "dt-album-shell-v3";
 const SHELL_FILES = [
   "./",
   "./index.html",
